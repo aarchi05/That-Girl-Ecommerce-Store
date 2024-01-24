9385f90876aa530fc11d44fbfe79af63e8017cb6
Aarchi Singh <a254sing@uwaterloo.ca>
2024-01-24 14:50:12 -0500

    edit summary h2 tag

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -140,7 +140,7 @@ const Summary = () => {
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
       <h2 className="text-lg font-medium text-gray-900">
-        Please click the "Confirm" button below to secure direct links to your products!
+        Please click the Confirm button below to secure direct links to your products!
       </h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
